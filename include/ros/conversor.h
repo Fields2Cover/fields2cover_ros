13fb9133825674073975fab268af2d0863cd879a
Vinny Ruia <vruia3@gatech.edu>
2022-11-26 16:01:37 -0500

    Add build support for the conversor for ros1 and ros2. Add Dockerfiles to verify the build

--- include/ros/conversor.h
+++ include/ros/conversor.h
@@ -6,30 +6,50 @@
 
 
 #include <fields2cover.h>
-#include <nav_msgs/Path.h>
-// #include <visualization_msgs/Marker.h>
-// #include <visualization_msgs/MarkerArray.h>
-#include <geometry_msgs/Polygon.h>
-#include <geometry_msgs/Pose.h>
-#include <geometry_msgs/Point32.h>
-#include <geometry_msgs/Point.h>
 #include <vector>
+
+#ifdef IS_ROS2
+  #include <nav_msgs/msg/path.hpp>
+  #include <geometry_msgs/msg/polygon.hpp>
+  #include <geometry_msgs/msg/pose.hpp>
+  #include <geometry_msgs/msg/point32.hpp>
+  #include <geometry_msgs/msg/point.h>
+#else
+  #include <nav_msgs/Path.h>
+  // #include <visualization_msgs/Marker.h>
+  // #include <visualization_msgs/MarkerArray.h>
+  #include <geometry_msgs/Polygon.h>
+  #include <geometry_msgs/Pose.h>
+  #include <geometry_msgs/Point32.h>
+  #include <geometry_msgs/Point.h>
+#endif
+
+
+namespace conversor {
+
+#ifdef IS_ROS2
+  namespace GeometryMsgs = geometry_msgs::msg;
+  namespace NavMsgs = nav_msgs::msg;
+#else
+  namespace GeometryMsgs = geometry_msgs;
+  namespace NavMsgs = nav_msgs;
+#endif
 
 class ROS {
  public:
-  static void to(const F2CPoint& _point, geometry_msgs::Point32& _p32);
-  static void to(const F2CPoint& _point, geometry_msgs::Point& _p64);
+  static void to(const F2CPoint& _point, GeometryMsgs::Point32& _p32);
+  static void to(const F2CPoint& _point, GeometryMsgs::Point& _p64);
 
   template <class T>
-  static void to(const T& _curve, geometry_msgs::Polygon& _poly);
+  static void to(const T& _curve, GeometryMsgs::Polygon& _poly);
 
   static void to(const F2CCell& _poly,
-    std::vector<geometry_msgs::Polygon>& _ros_poly);
+    std::vector<GeometryMsgs::Polygon>& _ros_poly);
 
   static void to(const F2CCells& _polys,
-    std::vector<std::vector<geometry_msgs::Polygon>>& _ros_polys);
+    std::vector<std::vector<GeometryMsgs::Polygon>>& _ros_polys);
 
-  static void to(const F2CLineString& _line, nav_msgs::Path& _path);
+  static void to(const F2CLineString& _line, NavMsgs::Path& _path);
 
   // static void to(const F2CLineString& _line,
   //  visualization_msgs::MarkerArray& _markers);
@@ -38,11 +58,12 @@ class ROS {
 };
 
 template <class T>
-inline void ROS::to(const T& _curve, geometry_msgs::Polygon& _poly) {
-  geometry_msgs::Point32 p32;
+inline void ROS::to(const T& _curve, GeometryMsgs::Polygon& _poly) {
+  GeometryMsgs::Point32 p32;
   for (const auto& p : _curve) {
     to(p, p32);
     _poly.points.push_back(p32);
   }
 }
 
+}  // namespace conversor
--- src/ros/conversor.cpp
+++ src/ros/conversor.cpp
@@ -7,14 +7,15 @@
 #include "ros/conversor.h"
 
 
-
-void ROS::to(const F2CPoint& _point, geometry_msgs::Point32& _p32) {
+namespace conversor
+{
+void ROS::to(const F2CPoint& _point, GeometryMsgs::Point32& _p32) {
   _p32.x = static_cast<float>(_point.getX());
   _p32.y = static_cast<float>(_point.getY());
   _p32.z = static_cast<float>(_point.getZ());
 }
 
-void ROS::to(const F2CPoint& _point, geometry_msgs::Point& _p64) {
+void ROS::to(const F2CPoint& _point, GeometryMsgs::Point& _p64) {
   _p64.x = _point.getX();
   _p64.y = _point.getY();
   _p64.z = _point.getZ();
@@ -22,8 +23,8 @@ void ROS::to(const F2CPoint& _point, geometry_msgs::Point& _p64) {
 
 
 void ROS::to(const F2CCell& _poly,
-    std::vector<geometry_msgs::Polygon>& _ros_poly) {
-  geometry_msgs::Polygon ros_ring;
+    std::vector<GeometryMsgs::Polygon>& _ros_poly) {
+  GeometryMsgs::Polygon ros_ring;
   to(_poly.getExteriorRing(), ros_ring);
   _ros_poly.push_back(ros_ring);
   const int n_in_rings = _poly.size();
@@ -35,20 +36,21 @@ void ROS::to(const F2CCell& _poly,
 }
 
 void ROS::to(const F2CCells& _polys,
-    std::vector<std::vector<geometry_msgs::Polygon>>& _ros_polys) {
+    std::vector<std::vector<GeometryMsgs::Polygon>>& _ros_polys) {
   for (auto&& p : _polys) {
-    std::vector<geometry_msgs::Polygon> poly;
+    std::vector<GeometryMsgs::Polygon> poly;
     to(p, poly);
     _ros_polys.push_back(poly);
   }
 }
 
-void ROS::to(const F2CLineString& _line, nav_msgs::Path& _path) {
-  geometry_msgs::PoseStamped pose;
+void ROS::to(const F2CLineString& _line, NavMsgs::Path& _path) {
+  GeometryMsgs::PoseStamped pose;
   for (auto&& p : _line) {
     to(p, pose.pose.position);
     _path.poses.push_back(pose);
   }
 }
 
+}  // namespace conversor
 
